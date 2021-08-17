631512d68daedb12ab0b59697c918a69b2376af7
thed3mir <fdemir2001@hotmail.com>
2021-08-17 23:54:39 +0300

    kendi çıkış butonunu yapma ve çıkmak istediğinden emin misin uyarısı

---
+++ kendi çıkış butonunu yapma .cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace görsel_prog_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult basilantuş;
+
+           basilantuş= MessageBox.Show("çıkmak istediğinden emin misin ? ","çıkış ekrenı",MessageBoxButtons.YesNo,MessageBoxIcon.Question,MessageBoxDefaultButton.Button2);
+            if (basilantuş == DialogResult.Yes) Application.Exit();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+           
+            this.button1.Location = new System.Drawing.Point(206, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            
+            this.ClientSize = new System.Drawing.Size(339, 350);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
